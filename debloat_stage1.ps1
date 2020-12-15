448d5068c48eb66ab49e73b02850cd9ff3c34a7b
Cristian Sava <CristianSava@safeguardglobal.com>
2020-12-15 22:38:35 +0200

    added script

--- debloat_stage1.ps1
+++ debloat_stage1.ps1
@@ -321,8 +321,8 @@ foreach ($key in $cdm) {
 Write-Host "Enabling Dark Mode"
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
 
-Write-Host "Set timezone and add language pack"
+Write-Host "Set timezone / add language pack"
 Set-TimeZone -Name "GTB Standard Time"
 $LanguageList = Get-WinUserLanguageList
 $LanguageList.Add("ro-RO")
-Set-WinUserLanguageList $LanguageList -force
+Set-WinUserLanguageList $LanguageList -Force
